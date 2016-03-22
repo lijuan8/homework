8b91c5c0893f6a42066a9327ee0db9498f81b06d
Lijuan Yang <lijuan.yang@searshc.com>
2016-03-21 21:35:07 -0700

    add codeship set up screen shot and minor chagne in test

--- src/test/java/com/ada/homework/ExpressionTest.java
+++ src/test/java/com/ada/homework/ExpressionTest.java
@@ -19,50 +19,46 @@ public class ExpressionTest {
 		Assert.assertEquals(7, tested.execute(input));
 	}
 
-	
 	@Test
 	public void testTwoEx() {
 		String input = "mult(add(2, 2), div(9, 3))";
 		Assert.assertEquals(12, tested.execute(input));
 	}
-	
+
 	@Test
 	public void testLetSecondEx() {
 		String input = "let(a, 5, add(a, a))";
 		Assert.assertEquals(10, tested.execute(input));
 	}
-	
-	
+
 	@Test
 	public void testLetSecondWithLet() {
 		String input = "let(a, 5, let(b, mult(a, 10), add(b, a)))";
 		Assert.assertEquals(55, tested.execute(input));
 	}
-	
+
 	@Test
 	public void testLetWithLets() {
 		String input = "let(a, let(b, 10, add(b, b)), let(b, 20, add(a, b)))";
 		Assert.assertEquals(40, tested.execute(input));
 	}
-	
-	@Test(expected=java.lang.RuntimeException.class)
+
+	@Test(expected = java.lang.RuntimeException.class)
 	public void testInvalidExpression() {
 		String input = "let(a, let(b, 10, add(b, b)), let(b, 20, add(a, b))";
 		Assert.assertEquals(40, tested.execute(input));
 	}
-	
-	@Test(expected=java.lang.RuntimeException.class)
+
+	@Test(expected = java.lang.RuntimeException.class)
 	public void testLetWithOnly2Parameters() {
 		String input = "let(a, let(b, 10), let(b, 20, add(a, b))";
 		tested.execute(input);
 	}
-	
-	@Test(expected=java.lang.RuntimeException.class)
+
+	@Test(expected = java.lang.RuntimeException.class)
 	public void testLetWithOnly2Parameters2() {
 		String input = "let(a, let(b, 10), let(b, add(a, b))";
 		tested.execute(input);
 	}
-	
-	
 
 }
--- src/test/java/com/ada/homework/UtilsTest.java
+++ src/test/java/com/ada/homework/UtilsTest.java
@@ -1,7 +1,6 @@
 package com.ada.homework;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UtilsTest {
